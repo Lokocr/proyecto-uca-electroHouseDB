a05dc9fe38dc933e3e43c40a8e003b6b9d9dbadf
Leyner Tencio <leinertenciog@gmail.com>
2025-10-21 21:42:56 -0600

    insertar datos de ejemplo para Clientes y Productos

--- electrohouse.sql
+++ electrohouse.sql
@@ -34,3 +34,22 @@ CREATE TABLE Productos (
     Stock INT CHECK (Stock >= 0)
 );
 GO
+
+
+-- ==========================================================
+-- INSERTAR DATOS DE EJEMPLO
+-- ==========================================================
+INSERT INTO Clientes (Nombre, Correo, Telefono, Direccion)
+VALUES 
+('Juan Pérez', 'juanp@gmail.com', '8888-1111', 'San José'),
+('María Gómez', 'maria.gomez@hotmail.com', '8877-2222', 'Heredia'),
+('Carlos Rodríguez', 'carlosr@gmail.com', '8855-3333', 'Cartago');
+GO
+
+INSERT INTO Productos (Nombre, Categoria, Precio, Stock)
+VALUES
+('Televisor Samsung 55"', 'Electrónica', 650.00, 10),
+('Laptop HP 15"', 'Computadoras', 800.00, 5),
+('Celular iPhone 13', 'Telefonía', 950.00, 8),
+('Parlante JBL', 'Audio', 120.00, 15);
+GO
